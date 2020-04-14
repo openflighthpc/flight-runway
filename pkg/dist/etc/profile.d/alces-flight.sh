2e01d2d04277e00e7d7dddcc28e14dea6b58dbfd
Mark J. Titorenko <mark.titorenko@alces-flight.com>
2020-04-14 18:05:08 +0100

    Improve robustness of flight runway profile script processing, specifically relating to `errexit` (`set -e`) option handling.
    If `set -e` is active, record that, disable it and turn it on again after processing.

--- pkg/dist/etc/profile.d/alces-flight.sh
+++ pkg/dist/etc/profile.d/alces-flight.sh
@@ -6,16 +6,25 @@
 ################################################################################
 export flight_ROOT=${flight_ROOT:-/opt/flight}
 if [ -d ${flight_ROOT}/etc/profile.d ]; then
+  # record the value of errexit
+  if [ "${-#*e}" != "$-" ]; then
+    _errexit_set=true
+  fi
+  shopt -s nullglob
   for i in ${flight_ROOT}/etc/profile.d/*.sh ; do
     if [ -r "$i" ]; then
-      if [ "${-#*i}" != "\$-" ]; then
+      # Ensure flight profile can't cause errexit
+      set +e
+      if [ "${-#*i}" != "$-" ]; then
         . "$i"
       else
         . "$i" >/dev/null 2>&1
       fi
-      # Ensure no flight profile leave errors on
-      set +e
     fi
   done
-  unset i
+  shopt -u nullglob
+  if [ "${_errexit_set}" == "true" ]; then
+    set -e
+  fi
+  unset i _set_errexit
 fi
